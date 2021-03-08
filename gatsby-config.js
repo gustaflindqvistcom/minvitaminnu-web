c4db14b370aaecc366d1c529bab2eb9ed079ff42
Gustaf Lindqvist <gustaf@tedgustaf.com>
2021-03-08 08:41:58 +0100

    Initial checking

---
+++ gatsby-config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  siteMetadata: {
+    title: "minvitaminnu",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "c882x319",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "264781362",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
