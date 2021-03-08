c7108a2179093ccf4131bcf5188acf66c5c029b0
Gustaf Lindqvist <gustaf@tedgustaf.com>
2021-03-08 23:01:57 +0100

    Slug

---
+++ src/pages/symptom.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../layouts/index"
+
+const Symptom = () => {
+    return (
+        <Layout>
+            <h1 className="text-8xl">Symptom</ h1>
+        </Layout>
+    )
+}
+
+export default Symptom
---
+++ src/pages/vitamin.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../layouts/index"
+
+const Vitamin = () => {
+    return (
+        <Layout>
+            <h1 className="text-8xl">Vitamin</ h1>
+        </Layout>
+    )
+}
+
+export default Vitamin
